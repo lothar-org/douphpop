e5ce99a7cee2cbd2bdba3aca5e3edeafd240396c
lothar <greatsen@sina.com>
2017-08-31 23:42:23 +0800

    local data

---
+++ data/config.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * WincomtechPHP
+ * --------------------------------------------------------------------------------------------------
+ * 版权所有 2013-2015 漳州豆壳网络科技有限公司，并保留所有权利。
+ * 网站地址: http://www.wowlothar.cn
+ * --------------------------------------------------------------------------------------------------
+ * 这不是一个自由软件！您只能在遵守授权协议前提下对程序代码进行修改和使用；不允许对程序代码以任何形式任何目的的再发布。
+ * 授权协议：http://www.wowlothar.cn/license.html
+ * --------------------------------------------------------------------------------------------------
+ * Author: Lothar
+ * Release Date: 2015-06-10
+*/
+
+$dbhost   = "localhost";
+$dbname   = "douphpop";
+$dbuser   = "root";
+$dbpass   = "hc@123";
+$prefix   = "dou_";
+
+// $dbhost   = "localhost";
+// $dbname   = "douphp";
+// $dbuser   = "root";
+// $dbpass   = "root";
+// $prefix   = "dou_";
+
+// charset
+define('DOU_CHARSET','utf-8');
+
+// administrator path
+define('ADMIN_PATH','admin');
+
+// mobile path
+define('M_PATH','m');
+
+
+?>
