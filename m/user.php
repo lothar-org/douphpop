a5a4db90863ea677b16c4bdadde584a0c6e2f5ca
luosader <greatsen@sina.com>
2017-11-04 09:00:24 +0800

    去除头部版权等信息注释

--- m/user.php
+++ m/user.php
@@ -1,16 +1,5 @@
 <?php
-/**
- * WincomtechPHP
- * --------------------------------------------------------------------------------------------------
- * 版权所有 2013-2035 XXX网络科技有限公司，并保留所有权利。
- * 网站地址: http://www.wowlothar.cn
- * --------------------------------------------------------------------------------------------------
- * 这不是一个自由软件！您只能在遵守授权协议前提下对程序代码进行修改和使用；不允许对程序代码以任何形式任何目的的再发布。
- * 授权协议：http://www.wowlothar.cn/license.html
- * --------------------------------------------------------------------------------------------------
- * Author: Lothar
- * Release Date: 2015-10-16
- */
+
 define('IN_LOTHAR', true);
 
 $sub = 'login|login_post|register|register_post|edit|edit_post|password|password_post|password_reset|password_reset_post|logout|order_list|order|order_cancel';
